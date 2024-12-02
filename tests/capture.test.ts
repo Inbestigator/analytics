d974591bcff0c7ce7447ff1a397249b60c41c47a
Inbestigator <inbestigatoryt@gmail.com>
2024-12-01 19:41:53 -0800

    Added auth

--- tests/capture.test.ts
+++ tests/capture.test.ts
@@ -2,9 +2,9 @@ import CaptureClient from "@capture/analytics";
 import { assertEquals } from "@std/assert";
 
 const client = new CaptureClient({
-  clientId: "test",
-  publicKey: "test",
-  privateKey: "test",
+  clientId: "cm46gwoy20000f8uw0temaosh",
+  publicKey: "cak_test",
+  privateKey: "cak_test_private",
   url: "http://localhost:8000",
 });
 
--- www/main.ts
+++ www/main.ts
@@ -1,4 +1,5 @@
-import { Application, Router } from "@oak/oak";
+import cuid from "cuid";
+import { Application, Context, Next, Router } from "@oak/oak";
 import { createClient } from "@libsql/client";
 import "@std/dotenv/load";
 
@@ -8,18 +9,70 @@ const client = createClient({
 });
 
 await client.execute(`
-  CREATE TABLE IF NOT EXISTS captures (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  message TEXT NOT NULL,
-  data TEXT DEFAULT NULL CHECK (json_valid(data))
+  CREATE TABLE IF NOT EXISTS accounts (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
   );
 `);
 
+await client.execute(`
+  CREATE TABLE IF NOT EXISTS keys (
+    id TEXT PRIMARY KEY,
+    key TEXT NOT NULL UNIQUE,
+    accountId TEXT NOT NULL REFERENCES accounts(id),
+    type TEXT NOT NULL CHECK (type IN ('public', 'private'))
+  );
+`);
+
+await client.execute(`
+  CREATE TABLE IF NOT EXISTS captures (
+    id TEXT PRIMARY KEY,
+    accountId TEXT NOT NULL REFERENCES accounts(id),
+    message TEXT NOT NULL,
+    data TEXT DEFAULT NULL CHECK (json_valid(data))
+  );
+`);
+
+const authenticate = async (
+  ctx: Context,
+  next: Next,
+  enforcePrivate: boolean = true,
+) => {
+  const key = ctx.request.headers.get("authorization");
+  const id = ctx.request.url.searchParams.get("id");
+
+  if (!key || !id) {
+    ctx.response.body = "Missing authorization or id";
+    ctx.response.status = 401;
+    return;
+  }
+
+  let sql = `SELECT * FROM keys WHERE key = ? AND accountId = ?`;
+
+  if (enforcePrivate) {
+    sql += " AND type = 'private'";
+  }
+
+  const { rows } = await client.execute({
+    sql,
+    args: [key, id],
+  });
+
+  if (rows.length === 0) {
+    ctx.response.body = "Invalid authorization or id";
+    ctx.response.status = 401;
+    return;
+  }
+
+  await next();
+};
+
 const router = new Router();
 
-router.get("/api/recap", async (ctx) => {
+router.get("/api/recap", authenticate, async (ctx) => {
   const params = ctx.request.url.searchParams;
   const messages = JSON.parse(params.get("messages") ?? "[]");
+  const id = params.get("id") ?? "";
 
   if (!Array.isArray(messages) || messages.length === 0) {
     ctx.response.body = "No messages";
@@ -31,8 +84,9 @@ router.get("/api/recap", async (ctx) => {
 
   try {
     const { rows } = await client.execute({
-      sql: `SELECT * FROM captures WHERE message IN (${placeholders})`,
-      args: messages,
+      sql:
+        `SELECT * FROM captures WHERE accountId = ? AND message IN (${placeholders})`,
+      args: [id, ...messages],
     });
 
     ctx.response.status = 200;
@@ -44,31 +98,41 @@ router.get("/api/recap", async (ctx) => {
   }
 });
 
-router.post("/api/capture", async (ctx) => {
-  const data = await ctx.request.body.json();
+router.post(
+  "/api/capture",
+  (ctx, next) => authenticate(ctx, next, false),
+  async (ctx) => {
+    const data = await ctx.request.body.json();
+    const id = ctx.request.url.searchParams.get("id") ?? "";
 
-  if (!("message" in data)) {
-    ctx.response.body = "Missing message";
-    ctx.response.status = 400;
-    return;
-  }
+    if (!("message" in data)) {
+      ctx.response.body = "Missing message";
+      ctx.response.status = 400;
+      return;
+    }
 
-  try {
-    await client.execute({
-      sql: `INSERT INTO captures (message, data) VALUES (?, ?)`,
-      args: [data.message, JSON.stringify(data.data) ?? null],
-    });
+    try {
+      await client.execute({
+        sql:
+          `INSERT INTO captures (id, accountId, message, data) VALUES (?, ?, ?, ?)`,
+        args: [cuid(), id, data.message, JSON.stringify(data.data) ?? null],
+      });
 
-    ctx.response.status = 200;
-    ctx.response.body = { success: true };
-  } catch (err) {
-    console.error(err);
-    ctx.response.status = 500;
-    ctx.response.body = { error: (err as Error).message };
-  }
-});
+      ctx.response.status = 200;
+      ctx.response.body = { success: true };
+    } catch (err) {
+      console.error(err);
+      ctx.response.status = 500;
+      ctx.response.body = { error: (err as Error).message };
+    }
+  },
+);
 
 const app = new Application();
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  await next();
+});
 app.use(router.routes());
 app.use(router.allowedMethods());
 
